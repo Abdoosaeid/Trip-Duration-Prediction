3d4f2fb6f69a95bfc797bff3b702d4202c2cf23f
Abdoosaeid <abdosaid2342004@gmail.com>
2025-10-13 22:33:25 +0200

    update train file

--- src/train.py
+++ src/train.py
@@ -1,46 +1,33 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_squared_error, r2_score
 import joblib
-import numpy as np
 from utils import column_transformation, prepare_data
 
 def Linear_regression(data_path):
-    # إعداد البيانات
+
     df, new_column = prepare_data(data_path, target_column='trip_duration')
 
-    # فصل الهدف (target)
+    print(
+        df.info()
+    )
+
     y_train = df[new_column]
     X_train = df.drop(columns=[new_column])
 
-    # تجهيز الـ column transformer على الداتا بدون الهدف
-    column_transformer, train_feature = column_transformation(X_train)
+    column_transformer, _ = column_transformation(X_train)
 
-    # بناء الـ pipeline
+
     pipeline = Pipeline(steps=[
         ('preprocessor', column_transformer),
         ('regression', LinearRegression())
     ])
 
-    # تدريب الموديل
+
     model = pipeline.fit(X_train, y_train)
 
-    # التقييم على نفس الداتا
-    y_pred = model.predict(X_train)
-    mse = mean_squared_error(y_train, y_pred)
-    rmse = np.sqrt(mse)
-    r2 = r2_score(y_train, y_pred)
 
-    print("📊 Model Performance on Training Data:")
-    print(f"   MSE  = {mse:.4f}")
-    print(f"   RMSE = {rmse:.4f}")
-    print(f"   R²   = {r2:.4f}")
-
-    # حفظ الموديل
     joblib.dump(model, r"D:\Trip-Duration-Prediction\models\model1_linear_regression.pkl")
     print("✅ Model saved successfully at models/model1_linear_regression.pkl")
-
-    return model, mse, rmse, r2
 
 
 if __name__ == "__main__":
